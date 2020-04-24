38c9c126735aa5c789dd853b1dd4c1b6164af454
Farais Razak <58303408+farais123@users.noreply.github.com>
2020-04-24 11:00:57 +0100

    Transaction

--- src/Controllers/Category.java
+++ src/Controllers/Category.java
@@ -2,6 +2,7 @@ package Controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -16,10 +17,11 @@ public class Category {
     public ListView<String> categoryLV;
 
 
-    public void confirmCategoryBtnClicked(javafx.event.ActionEvent actionEvent) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("../FXML/activity_category.fxml"));
+    public void confirmCategoryBtnClicked(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../FXML/activity_category.fxml"));
+        Scene scene = new Scene(loader.load());
         Stage stage = new Stage();
-        stage.setScene(new Scene(root));
+        stage.setScene(scene);
         stage.show();
 
         ((Stage) confirmCategoryBtn.getScene().getWindow()).close();
--- src/Controllers/Transaction.java
+++ src/Controllers/Transaction.java
@@ -1,20 +1,34 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Transaction {
     public Button confirmTransactionBtn;
+    public TextField amountTF;
+    public Button selectCategoryBtn;
+    public TextArea noteTA;
+    public ToggleButton incomeToggleBtn;
+    public ToggleButton expenseToggleBtn;
+    public TextField dateTF;
 
-    public void confirmTransactionBtnClicked(javafx.event.ActionEvent actionEvent) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("../FXML/activity_main.fxml"));
+
+
+    public void confirmTransactionBtnClicked(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../FXML/activity_main.fxml"));
+        Scene scene = new Scene(loader.load());
         Stage stage = new Stage();
-        stage.setScene(new Scene(root));
+        stage.setScene(scene);
         stage.show();
     }
 }
